492f737cfdce354b2e12e02ffd3e2cbcadacf447
rigueti-bruno <bruno.rigueti@outlook.com>
2022-06-14 23:24:02 -0300

    começando a aprender JavaScript

---
+++ JAVASCRIPT/01-Introdução ao Javascript/teste.js
@@ -0,0 +1,23 @@
+// var y = 0; => comentário de uma linha
+
+/* var x = 1;
+   var z = 2; => bloco de código comentado */
+
+//var desconto = 0.2;
+
+//const PRECO = 2;
+
+//total = PRECO - desconto;
+
+function soma (a, b) {
+    console.log(a + b);
+}
+
+soma(3, 5);
+
+
+function mult (a, b) {
+    return a * b;
+}
+
+console.log(mult(4, 5))
