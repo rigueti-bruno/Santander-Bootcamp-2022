247fa5859540080d24a5b4d6f710b40e876c60da
rigueti-bruno <bruno.rigueti@outlook.com>
2022-07-06 17:00:34 -0300

    Troca da API - sem sucesso

--- JAVASCRIPT/09-JavaScript Assincrono/assets/js/scripts.js
+++ JAVASCRIPT/09-JavaScript Assincrono/assets/js/scripts.js
@@ -1,11 +1,11 @@
-const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+const BASE_URL = 'https://api.thecatapi.com/v1/images/search/';
 
 const getCats = async() => {
     const data = await fetch(BASE_URL)
     .then(res => res.json())
     .catch(e => console.log(e))
 
-    return data.webpurl;
+    return data.url;
 };
 
 const loadImg = async() => {
