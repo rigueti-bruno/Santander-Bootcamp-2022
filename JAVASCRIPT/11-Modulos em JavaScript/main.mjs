80d17c95b8e72e0350baafdcb22979d7094bcf8f
rigueti-bruno <bruno.rigueti@outlook.com>
2022-07-07 17:14:06 -0300

    Modulos no JavaScript

---
+++ JAVASCRIPT/11-Modulos em JavaScript/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Bruno', 35));
+console.log(mostraCidade('Bruno', 'Itaperuna'));
+console.log(mostraHobby('Bruno', 'Ler'));
